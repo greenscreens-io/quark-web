cfa026eb0ee7bf33f4e7c0cfeb0de720b62b3bf6
unknown <greenscreens.ltd@gmail.com>
2020-09-18 18:49:02 +0200

    Add compress support flag support for binary socket data

--- src/core/streams.js
+++ src/core/streams.js
@@ -8,7 +8,8 @@
 class Streams {
 
 	static get isAvailable() {
-		return typeof CompressionStream !== 'undefined' && typeof DecompressionStream !== 'undefined';
+		return typeof CompressionStream !== 'undefined' &&
+				typeof DecompressionStream !== 'undefined';
 	}
 
 	static async compress(text, encoding = 'gzip') {
